31d344fbb8517a3492bffa893eb9f762fb02520b
Mark Graves <mark.graves@tylertech.com>
2022-12-12 10:25:25 -0600

    feat: Finish integration tests

--- stopwatch-manager.IntegrationTests/Program.cs
+++ stopwatch-manager.IntegrationTests/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Logging;
+using Xunit;
 
 namespace stopwatch_manager.integration_tests;
 
@@ -6,6 +7,15 @@ public partial class Program
 {
     public static void Main()
     {
+        TestWithLogsNoCustomPrefix();
+        TestWithLogsAndCustomPrefix();
+        TestNoLogsNoCustomPrefix();
+        TestNoLogsAndCustomPrefix();
+    }
+
+    private static void TestWithLogsNoCustomPrefix()
+    {
+        // Test case: ILogger used; default log prefixes
         using var loggerFactory =
         LoggerFactory.Create(builder =>
             builder.AddSimpleConsole(options =>
@@ -18,27 +28,42 @@ public partial class Program
         var logger = loggerFactory.CreateLogger<Program>();
 
         var stopwatchManager = new StopwatchManager(logger);
-        var started = stopwatchManager.TryStart(out var key1);
-        if (!started)
-            Console.WriteLine("Stopwatch 1 was not started");
-        Thread.Sleep(1000);
-        stopwatchManager.TryStart(out var key2);
-        Thread.Sleep(1000);
-        stopwatchManager.TryStart(out var key3);
-        Thread.Sleep(1000);
-        stopwatchManager.TryStart(out var key4);
-        Thread.Sleep(1000);
+
+        Assert.True(stopwatchManager.TryStart(out var key1));
+        Assert.NotNull(key1);
+        Assert.Equal(key1, $"TestWithLogsNoCustomPrefix_{(new System.Diagnostics.StackFrame(0, true)).GetFileLineNumber() - 2}");
+        Thread.Sleep(100);
+
+        Assert.True(stopwatchManager.TryStart(out var key2));
+        Assert.NotNull(key2);
+        Assert.Equal(key2, $"TestWithLogsNoCustomPrefix_{(new System.Diagnostics.StackFrame(0, true)).GetFileLineNumber() - 2}");
+        Thread.Sleep(100);
+
+        Assert.True(stopwatchManager.TryStart(out var key3));
+        Assert.NotNull(key3);
+        Assert.Equal(key3, $"TestWithLogsNoCustomPrefix_{(new System.Diagnostics.StackFrame(0, true)).GetFileLineNumber() - 2}");
+        Thread.Sleep(100);
+
+        Assert.True(stopwatchManager.TryStart(out var key4));
+        Assert.NotNull(key4);
+        Assert.Equal(key4, $"TestWithLogsNoCustomPrefix_{(new System.Diagnostics.StackFrame(0, true)).GetFileLineNumber() - 2}");
+        Thread.Sleep(100);
+
         stopwatchManager.TryStart(out var key5);
-        Thread.Sleep(1000);
+        Assert.NotNull(key5);
+        Assert.Equal(key5, $"TestWithLogsNoCustomPrefix_{(new System.Diagnostics.StackFrame(0, true)).GetFileLineNumber() - 2}");
+        Thread.Sleep(100);
+
         stopwatchManager.LogStopwatchList();
-        stopwatchManager.Reset(key1);
-        stopwatchManager.TryStart(key1);
-        stopwatchManager.TryStop(key1);
-        stopwatchManager.TryStopAndRemove(key2);
-        stopwatchManager.Restart(key3);
-        stopwatchManager.TryStop(key3);
-        stopwatchManager.TryStop(key4);
-        stopwatchManager.TryStop(key5);
-        var stopwatchKeys = stopwatchManager.GetStopwatchKeys();
+
+        Assert.True(stopwatchManager.Reset(key1));
+        Assert.True(stopwatchManager.TryStart(key1));
+        Assert.True(stopwatchManager.TryStop(key1));
+        Assert.True(stopwatchManager.TryStopAndRemove(key2));
+        Assert.True(stopwatchManager.Restart(key3));
+        Assert.True(stopwatchManager.TryStop(key3));
+        Assert.True(stopwatchManager.TryStop(key4));
+        Assert.True(stopwatchManager.TryStop(key5));
+        Assert.Equivalent(new List<string> { key1, key3, key4, key5 }, stopwatchManager.GetStopwatchKeys());
     }
 }
