9849ea99a7e2a5104b515b446b7ae1d1f395e6c6
Mark Graves <mark.graves@tylertech.com>
2022-12-16 11:58:35 -0600

    chore: Fixed formatting issues

--- stopwatch-manager/TimespanGranularities.cs
+++ stopwatch-manager/TimespanGranularities.cs
@@ -3,9 +3,9 @@ namespace stopwatch_manager;
 /// <summary>Enumeration that determines the type of measurement that appears in logs</summary>
 public enum TimespanGranularity
 {
-  /// <summary>The number of elapsed ticks</summary>
-  Ticks = 0,
-  /// <summary>The number of elapsed milliseconds</summary>
-  Milliseconds,
+    /// <summary>The number of elapsed ticks</summary>
+    Ticks = 0,
+    /// <summary>The number of elapsed milliseconds</summary>
+    Milliseconds,
 
 }
